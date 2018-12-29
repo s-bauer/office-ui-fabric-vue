fad4c54de773ede241be652eb5caae1db72df1c5
Simon Bauer <bauer-s-public@outlook.com>
2018-12-29 14:15:51 +0100

    Added index.js / Adjusted package.json / Renamed OfficeDefaultButton to OfficeButton

---
+++ src/components/index.ts
@@ -0,0 +1,22 @@
+import OfficeButton from "@/components/Button/OfficeButton.vue";
+import OfficeCheckbox from "@/components/Checkbox/OfficeCheckbox.vue";
+import OfficeChoiceGroup from "@/components/ChoiceGroup/OfficeChoiceGroup.vue";
+import OfficeIcon from "@/components/Icon/OfficeIcon.vue";
+import OfficeImage from "@/components/Image/OfficeImage.vue";
+import OfficeLabel from "@/components/Label/OfficeLabel.vue";
+import OfficeTextField from "@/components/TextField/OfficeTextField.vue";
+import {Vue} from "vue/types/vue";
+
+const components: { [id: string]: any } = {
+    OfficeButton,
+    OfficeCheckbox,
+    OfficeChoiceGroup,
+    OfficeIcon,
+    OfficeImage,
+    OfficeLabel,
+    OfficeTextField
+};
+
+Object.keys(components).forEach((name) => {
+    Vue.component(name, components[name]);
+});
