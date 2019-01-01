b53c3f92fdd98f3a9235a314a7b3f5ca9c51c653
Simon Bauer <bauer-s-public@outlook.com>
2019-01-02 00:32:38 +0100

    Adjusted index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,12 @@
 import OfficeButton from "@/components/Button/OfficeButton.vue";
 import OfficeCheckbox from "@/components/Checkbox/OfficeCheckbox.vue";
 import OfficeChoiceGroup from "@/components/ChoiceGroup/OfficeChoiceGroup.vue";
+import FocusTrapZone from "@/components/FocusTrapZone/FocusTrapZone.vue";
 import OfficeIcon from "@/components/Icon/OfficeIcon.vue";
 import OfficeImage from "@/components/Image/OfficeImage.vue";
 import OfficeLabel from "@/components/Label/OfficeLabel.vue";
+import OfficeLink from "@/components/Link/OfficeLink.vue";
+import OfficeSlider from "@/components/Slider/OfficeSlider.vue";
 import OfficeTextField from "@/components/TextField/OfficeTextField.vue";
 import OfficeToggle from "@/components/Toggle/OfficeToggle.vue";
 import Vue from "vue";
@@ -12,11 +15,14 @@ const components: { [id: string]: any } = {
     OfficeButton,
     OfficeCheckbox,
     OfficeChoiceGroup,
+    FocusTrapZone,
     OfficeIcon,
     OfficeImage,
     OfficeLabel,
+    OfficeLink,
+    OfficeSlider,
     OfficeTextField,
-    OfficeToggle
+    OfficeToggle,
 };
 
 Object.keys(components).forEach((name) => {
