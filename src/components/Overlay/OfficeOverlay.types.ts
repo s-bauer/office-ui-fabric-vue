86454325535456982a84fb2ef6c71406dd9144bd
smoti123 <mathis.michel@outlook.de>
2019-01-02 13:48:08 +0100

    remove unused labels

--- src/components/Overlay/OfficeOverlay.types.ts
+++ src/components/Overlay/OfficeOverlay.types.ts
@@ -3,25 +3,9 @@ import {ITheme} from "@/styling";
 
 export interface IOfficeOverlayProps {
     /**
-     * Gets the component ref.
-     */
-    componentRef?: object;
-
-    /**
-     * Call to provide customized styling that will layer on top of the variant rules
-     */
-    styles?: IStyleFunctionOrObject<IOfficeOverlayStyleProps, IOfficeOverlayStyles>;
-
-    /**
      * Theme provided by HOC.
      */
     theme?: ITheme;
-
-    /**
-     * Additional css class to apply to the Overlay
-     * @defaultvalue undefined
-     */
-    className?: string;
 
     /**
      * Whether to use the dark-themed overlay.
@@ -37,11 +21,6 @@ export interface IOfficeOverlayStyleProps {
      * Accept theme prop.
      */
     theme: ITheme;
-
-    /**
-     * Accept custom classNames
-     */
-    className?: string;
 
     /**
      * Is overlay visible
