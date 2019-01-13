efda5b667b5dad5de108e3aa80f5dbfad2ee1b4d
smoti123 <mathis.michel@outlook.de>
2019-01-13 20:29:11 +0100

    refactor

--- packages/office-ui-fabric-vue/components/Overlay/OfficeOverlay.vue
+++ packages/office-ui-fabric-vue/components/Overlay/OfficeOverlay.vue
@@ -24,6 +24,10 @@
             overflow: "hidden !important" as "hidden"
         });
 
+        public destroyed() {
+            this.enableBodyScroll();
+        }
+
         public get classNames() {
             return mergeStyleSets(getStyles({
                 isDark: this.isDarkThemed,
@@ -58,8 +62,7 @@
                 doc.body.removeEventListener("touchmove", this.disableIosBodyScroll);
             }
         }
-        public destroyed(){
-            this.enableBodyScroll();
-        }
+
+
     }
 </script>
