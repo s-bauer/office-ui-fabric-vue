d4fe98b3ca2f9c1847df7bf6bba122cd04573eb0
Simon Bauer <bauer-s-public@outlook.com>
2019-11-16 20:21:19 +0100

    More fixes to get everything to work

---
+++ packages/documentation/types/highlight.d.ts
@@ -0,0 +1,7 @@
+declare module 'highlight.js/lib/languages/shell';
+declare module 'highlight.js/lib/languages/javascript';
+declare module 'highlight.js/lib/languages/xml';
+declare module 'highlight.js' {
+    export interface HLJSStatic {}
+    export interface IModeBase {}
+}
